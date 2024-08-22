5835c75ddcc647c6c5b7731585b9c1e1dd078039
Kamil Ratowski <kamilratowski90@gmail.com>
2024-08-22 13:46:49 +0200

    Initial commit with spring boot setup generated by Spring Initializr

---
+++ src/main/java/com/kratowski/todo_list_rest_api_spring_boot/TodoListRestApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.kratowski.todo_list_rest_api_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListRestApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListRestApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kratowski/todo_list_rest_api_spring_boot/TodoListRestApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kratowski.todo_list_rest_api_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListRestApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
