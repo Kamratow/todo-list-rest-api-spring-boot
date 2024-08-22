bb5346f68e006c1c9e381b5c68a690221b30812f
Kamil Ratowski <kamilratowski90@gmail.com>
2024-08-23 00:24:31 +0200

    Removed unnecessary import statements from TaskService

--- src/main/java/com/kratowski/todo_list_rest_api_spring_boot/service/TaskService.java
+++ src/main/java/com/kratowski/todo_list_rest_api_spring_boot/service/TaskService.java
@@ -2,9 +2,7 @@ package com.kratowski.todo_list_rest_api_spring_boot.service;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.kratowski.todo_list_rest_api_spring_boot.model.Task;
 import com.kratowski.todo_list_rest_api_spring_boot.repository.TaskRepository;
