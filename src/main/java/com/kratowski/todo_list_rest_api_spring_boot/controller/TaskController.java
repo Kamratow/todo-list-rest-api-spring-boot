01ad14e0547e952cb1e2ad485352850dccb08ee7
Kamil Ratowski <kamilratowski90@gmail.com>
2024-08-23 00:23:42 +0200

    Added handling request for updating single task

--- src/main/java/com/kratowski/todo_list_rest_api_spring_boot/controller/TaskController.java
+++ src/main/java/com/kratowski/todo_list_rest_api_spring_boot/controller/TaskController.java
@@ -2,16 +2,19 @@ package com.kratowski.todo_list_rest_api_spring_boot.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.kratowski.todo_list_rest_api_spring_boot.model.Task;
 import com.kratowski.todo_list_rest_api_spring_boot.service.TaskService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class TaskController {
@@ -42,4 +45,12 @@ public class TaskController {
         return taskService.createTask(task);
     }
 
+    @PutMapping("task/{id}")
+    public @ResponseBody Task putMethodName(@PathVariable String id, @RequestBody Task task) {
+        if (task.getId() == 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Please provide task id");
+        }
+
+        return taskService.updateTask(task);
+    }
 }
--- src/main/java/com/kratowski/todo_list_rest_api_spring_boot/service/TaskService.java
+++ src/main/java/com/kratowski/todo_list_rest_api_spring_boot/service/TaskService.java
@@ -2,7 +2,9 @@ package com.kratowski.todo_list_rest_api_spring_boot.service;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.kratowski.todo_list_rest_api_spring_boot.model.Task;
 import com.kratowski.todo_list_rest_api_spring_boot.repository.TaskRepository;
@@ -25,6 +27,10 @@ public class TaskService {
     }
 
     public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task updateTask(Task task) {
         return taskRepository.save(task);
     }
 }
